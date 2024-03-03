79bb3952b41602df097b6d4cac30a70f957bd776
Naman Thanki <stuniz7855@gmail.com>
2024-03-03 22:40:53 +0530

    refresh token route path added

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -5,7 +5,8 @@ import { verifyAccessToken } from "../middlewares/auth.middleware.js";
 
 authRouter.post("/register", AuthController.register);
 authRouter.post("/login", AuthController.login);
-authRouter.post("/logout", verifyAccessToken, AuthController.logout);
 authRouter.post("/refresh-token", AuthController.refreshToken);
+authRouter.post("/logout", verifyAccessToken, AuthController.logout);
+authRouter.post("/change-password", verifyAccessToken, AuthController.changePassword);
 
 export default authRouter;
